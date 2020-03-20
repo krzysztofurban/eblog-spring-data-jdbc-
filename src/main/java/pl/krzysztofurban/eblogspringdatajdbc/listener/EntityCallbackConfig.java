a24a4aab769f7435213a197d0c6ac04ddd638bea
Krzysztof Urban <k.urban@g.pl>
2020-03-20 18:04:58 +0100

    Add entity callback example

---
+++ src/main/java/pl/krzysztofurban/eblogspringdatajdbc/listener/EntityCallbackConfig.java
@@ -0,0 +1,19 @@
+package pl.krzysztofurban.eblogspringdatajdbc.listener;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.relational.core.mapping.event.AfterLoadCallback;
+import pl.krzysztofurban.eblogspringdatajdbc.model.Author;
+
+@Configuration
+@Slf4j
+public class EntityCallbackConfig {
+  @Bean
+  AfterLoadCallback<Author> afterLoadCallbackExample() {
+    return it -> {
+      log.info("AfterLoadCallback: {}", it.toString());
+      return it;
+    };
+  }
+}
